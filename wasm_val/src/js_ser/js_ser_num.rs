4ced21405c74fb3a1c98766f52e6ce49b3c7b60a
Vladimir Iftodi <vladimir.iftodi@gmail.com>
2018-07-18 21:21:57 +0200

    Split js_ser module into multiple files

--- wasm_val/src/js_ser/js_ser_num.rs
+++ wasm_val/src/js_ser/js_ser_num.rs
@@ -7,25 +7,11 @@
 // except according to those terms.
 
 use std::io::Cursor;
+
+use super::JsSerializable;
 use ser_constants::TypeTag;
 
 use byteorder::{LittleEndian, WriteBytesExt,};
-
-pub trait JsSerializable {
-    fn size(&self) -> u8;
-    fn ser(&self, cursor: &mut Cursor<Vec<u8>>) -> ();
-}
-
-impl JsSerializable for bool {
-    fn size(&self) -> u8 { 1 }
-
-    fn ser(&self, cursor: &mut Cursor<Vec<u8>>) {
-        match self {
-            false => cursor.write_u8(TypeTag::BoolFalse as u8).unwrap(),
-            true => cursor.write_u8(TypeTag::BoolTrue as u8).unwrap(),
-        }
-    }
-}
 
 impl JsSerializable for i8 {
     fn size(&self) -> u8 { 2 }
@@ -96,15 +82,5 @@ impl JsSerializable for f64 {
     fn ser(&self, cursor: &mut Cursor<Vec<u8>>) {
         cursor.write_u8(TypeTag::F64 as u8).unwrap();
         cursor.write_f64::<LittleEndian>(*self).unwrap();
-    }
-}
-
-impl<'a> JsSerializable for &'a str {
-    fn size(&self) -> u8 { 9 }
-
-    fn ser(&self, cursor: &mut Cursor<Vec<u8>>) {
-        cursor.write_u8(TypeTag::String as u8).unwrap();
-        cursor.write_u32::<LittleEndian>(self.len() as u32).unwrap();
-        cursor.write_u32::<LittleEndian>(self.as_ptr() as u32).unwrap();
     }
 }
---
+++ wasm_val/src/js_ser/mod.rs
@@ -0,0 +1,41 @@
+// Copyright 2018 Vladimir Iftodi <Vladimir.Iftodi@gmail.com>. 
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::io::Cursor;
+use ser_constants::TypeTag;
+
+use byteorder::{LittleEndian, WriteBytesExt,};
+
+mod js_ser_num;
+
+pub trait JsSerializable {
+    fn size(&self) -> u8;
+    fn ser(&self, cursor: &mut Cursor<Vec<u8>>) -> ();
+}
+
+impl JsSerializable for bool {
+    fn size(&self) -> u8 { 1 }
+
+    fn ser(&self, cursor: &mut Cursor<Vec<u8>>) {
+        match self {
+            false => cursor.write_u8(TypeTag::BoolFalse as u8).unwrap(),
+            true => cursor.write_u8(TypeTag::BoolTrue as u8).unwrap(),
+        }
+    }
+}
+
+
+impl<'a> JsSerializable for &'a str {
+    fn size(&self) -> u8 { 9 }
+
+    fn ser(&self, cursor: &mut Cursor<Vec<u8>>) -> () {
+        cursor.write_u8(TypeTag::String as u8).unwrap();
+        cursor.write_u32::<LittleEndian>(self.len() as u32).unwrap();
+        cursor.write_u32::<LittleEndian>(self.as_ptr() as u32).unwrap();
+    }
+}
